a538a38548e120bae49d2ba1963bde73a303775c
Ethan Cragun <craguned@gmail.com>
2018-09-05 14:30:34 -0600

    Broke Character Controller

--- 2670Project/Assets/CharacterControlScripts/CharacterMovement.cs
+++ 2670Project/Assets/CharacterControlScripts/CharacterMovement.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour {
+[RequiredComponent(typeof(CharacterController))]
 
+public class CharacterMovement : MonoBehaviour 
+{
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
+
+    public FloatData MoveX, MoveY, MoveZ;
+
     private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
     void Update() {
-        CharacterController controller = GetComponent<CharacterController>();
+     
         if (controller.isGrounded) {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.value);
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
-            
+     
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
