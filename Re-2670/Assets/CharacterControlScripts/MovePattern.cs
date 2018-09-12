1408d18ee5d6b6b343750f90660768a7de5481e1
Ethan Cragun <craguned@gmail.com>
2018-09-12 14:20:38 -0600

    GAHH following Romrell

--- Re-2670/Assets/CharacterControlScripts/MovePattern.cs
+++ Re-2670/Assets/CharacterControlScripts/MovePattern.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu]
 public class MovePattern : ScriptableObject {
 
-    public float jumpSpeed;
     public float gravity;
     
     public FloatData MoveX, MoveY, MoveZ;
@@ -14,17 +11,26 @@ public class MovePattern : ScriptableObject {
     private Vector3 moveDirection;
     private Vector3 rotDirection;
 
-    public coid Invoke(CharacterController controller, Transform transform)
+    public virtual void Invoke(CharacterController controller, Transform transform)
     {
-        if (controller.isGrounded) {
-            moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.value);
+        if (controller.isGrounded)
+        {
+            Move(transform)
+        }
+
+        Move(controller)
+    }
+
+    protected void Move(CharacterController controller)
+    {
+        moveDirection.y -= gravity.Value * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+        {
+            
+    moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.value);
             rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
             transform.Rotate(rotDirection);
             moveDirection = transform.TransformDirection(moveDirection);
-
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed.Value;
-     
         }
         moveDirection.y -= gravity.Value * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime); 
---
+++ Re-2670/Assets/CharacterControlScripts/MovePatternDual.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileNAme = "MovePatternDual")]
+public class MovePatternDual : MovePattern
+{
+	private bool doubleJump;
+
+	public override void Invoke(CharacterController controller, Transform transform)
+	{
+		if (controller.isGrounded)
+		{
+			Move(transform);			
+			doubleJump = true;
+		}
+		else
+		{
+			
+			if (doubleJump)
+			{
+				move(transform);
+				doubleJump = false;
+			}
+		}
+
+		Move(controller);
+	}
+}
