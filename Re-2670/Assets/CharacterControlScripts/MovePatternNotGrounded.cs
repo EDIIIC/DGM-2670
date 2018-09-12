b1010372a720561d4018bca28ad3ee9f5be1c33e
Ethan Cragun <craguned@gmail.com>
2018-09-12 14:32:24 -0600

    Gah

--- Re-2670/Assets/CharacterControlScripts/MovePatternNotGrounded.cs
+++ Re-2670/Assets/CharacterControlScripts/MovePatternNotGrounded.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "MovePatternNotGrounded")]
 public class MovePatternNotGrounded : MovePattern {
