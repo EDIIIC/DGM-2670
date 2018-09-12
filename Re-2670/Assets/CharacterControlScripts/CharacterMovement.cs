bc164c334d685ccf61c4aa01a66ff2dad6182552
Ethan Cragun <craguned@gmail.com>
2018-09-12 13:18:29 -0600

    Created New 2670 Project
    Still doesnt like me

---
+++ Re-2670/Assets/CharacterControlScripts/CharacterMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class CharacterMovement : MonoBehaviour
+{
+
+	private CharacterController controller;
+	public MovePattern Pattern;
+	
+
+	void Start ()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update ()
+	{
+		Pattern().Invoke(controller, transform);
+	}
+	
+}
