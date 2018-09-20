5d527a7129bf3220d069b791cd1eafb86f3def77
Ethan Cragun <craguned@gmail.com>
2018-09-20 15:06:11 -0600

    Level Manager and text edits

---
+++ Re-2670/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public static int PlayerCount;
+
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+	//		LoadNextLevel ();
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex - 1);
+		}
+	}
+		
+	public void LevelLoad (string lvl) {
+		SceneManager.LoadScene (lvl);
+	}
+		
+	public void ExitGame (){
+		print ("Quit game.");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void CheckPlayerCount () {
+		if (PlayerCount <= 0) {
+			LoadNextLevel ();
+		}
+	}
+
+}
+	
--- Re-2670/Assets/Scripts/MoveScripts/CharacterMovement.cs
+++ Re-2670/Assets/Scripts/MoveScripts/CharacterMovement.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(CharacterController))]
-
 public class CharacterMovement : MonoBehaviour
 {
-
+	public int health;
+	public GameObject levelManager;
 	private CharacterController controller; //moves object
 	public MovePattern Pattern;
 	
