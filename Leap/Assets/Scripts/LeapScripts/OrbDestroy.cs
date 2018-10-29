60b4a950d51f7deb5da51225d8b26ae76f70d836
Ethan Cragun <craguned@gmail.com>
2018-10-29 14:34:33 -0600

    GameAesthetic

---
+++ Leap/Assets/Scripts/LeapScripts/OrbDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbDestroy : MonoBehaviour {
+    
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Orb(Clone)")
+        {
+            ScoreBoard.scoreValue += 1;
+            Destroy(col.gameObject);
+        }
+    }
+}
