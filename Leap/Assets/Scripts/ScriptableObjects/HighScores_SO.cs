7b8837aa5a8619a291d64fd8b49036c3fcca56be
Ethan Cragun <craguned@gmail.com>
2018-11-02 23:55:08 -0600

    Making scriptable object Highscore
    Or at least started trying to make it

--- Leap/Assets/Scripts/ScriptableObjects/HighScores_SO.cs
+++ Leap/Assets/Scripts/ScriptableObjects/HighScores_SO.cs
@@ -1,4 +1,50 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+[CreateAssetMenu (fileName = "HighScore")]
+
+public class HighScores_SO : ScriptableObject
+{
+    public List<int> highScores;
+    public int score;
+//    public Text currentDisplay;
+//    public Text highscoreDisplay;
+    
+    
+    public void OrderScores()
+    {
+        //does stuff?
+    }
+    
+//    
+    public void SaveScore()
+    {
+        //Check previous score
+        int oldScore = GetScore();
+
+        //if new score is higher than previous score
+        if(score > oldScore)
+            PlayerPrefs.SetInt ("HighScore", score);
+    }
+
+    
+    public int GetScore()
+    {
+        return PlayerPrefs.GetInt ("HighScore");
+    }
+
+    
+    public void OnDisable()
+    {
+        SaveScore ();
+    }
+//
+   
+}
+
+/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,3 +60,4 @@ public class HighScores_SO : ScriptableObject
     }
     
 }
+*/
