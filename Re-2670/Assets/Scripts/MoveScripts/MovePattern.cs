09215d510e4eb61be05f776c454d9d075af2f126
Ethan Cragun <craguned@gmail.com>
2018-11-17 01:00:01 -0700

    Script Google Doc
    Edited some scripts to make them more general and compact

--- Re-2670/Assets/Scripts/MoveScripts/MovePattern.cs
+++ Re-2670/Assets/Scripts/MoveScripts/MovePattern.cs
@@ -37,53 +37,6 @@ public class MovePattern : ScriptableObject
     }
 }
 
-
-
-
-
-
-
-
 /*
-// BROKEN
-using UnityEngine;
 
-[CreateAssetMenu]
-public class MovePattern : ScriptableObject 
-{ 
-    public FloatData JumpSpeed;
-    public FloatData Gravity;
-    
-    public FloatData MoveX, MoveY, MoveZ;
-    public FloatData RotX, RotY, RotZ;
-
-    private Vector3 moveDirection;
-    private Vector3 rotDirection;
-
-    public virtual void Invoke(CharacterController controller, Transform transform)
-    {
-        if (controller.isGrounded)
-        {
-            Move(transform);
-        }
-
-        Move(controller);
-    }
-
-    protected void Move(CharacterController controller)
-    {
-        Move(controller);
-       // moveDirection.y -= gravity.Value * Time.deltaTime;
-       // controller.Move(moveDirection * Time.deltaTime);
-    }
-
-    protected void Move(Transform transform)
-    {
-        moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.value);
-        rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-        transform.Rotate(rotDirection);
-        moveDirection = transform.TransformDirection(moveDirection);
-    }
-
-}
 */
