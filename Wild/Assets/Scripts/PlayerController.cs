81f2547783351dfc32a7d3928280e87f13d9ce17
Ethan Cragun <craguned@gmail.com>
2018-11-23 16:19:38 -0700

    Test Scene, Object Detail, twitch and fire issues

--- Wild/Assets/Scripts/PlayerController.cs
+++ Wild/Assets/Scripts/PlayerController.cs
@@ -4,41 +4,41 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    CharacterController characterController;
+   CharacterController characterController;
 
-    public float speed;
-    public float jumpSpeed;
-    public float gravity;
-    private Vector3 moveDirection = Vector3.zero;
-    //public AudioSource audioSource;
+   public float speed;
+   public float jumpSpeed;
+   public float gravity;
+   private Vector3 moveDirection = Vector3.zero;
+   //public AudioSource audioSource;
 
-    private CharacterController controller;
+   private CharacterController controller;
 
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-    }
+   void Start()
+   {
+      controller = GetComponent<CharacterController>();
+   }
 
-    void Update()
-    {
+   void Update()
+   {
 
-        if (controller.isGrounded)
-        {       
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection = moveDirection * speed;
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
-        }
-        else
-        {
+      if (controller.isGrounded)
+      {       
+         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+         moveDirection = transform.TransformDirection(moveDirection);
+         moveDirection = moveDirection * speed;
+         if (Input.GetButton("Jump"))
+         {
+            moveDirection.y = jumpSpeed;
+         }
+      }
+      else
+      {
 //       Left/Right movement in mid air allowed
-            moveDirection.x = Input.GetAxis("Horizontal") * speed;
-        }
-        moveDirection.y -= gravity * Time.deltaTime;
+         moveDirection.x = Input.GetAxis("Horizontal") * speed;
+      }
+      moveDirection.y -= gravity * Time.deltaTime;
       
-        controller.Move(moveDirection * Time.deltaTime);
-    }
+      controller.Move(moveDirection * Time.deltaTime);
+   }
 }
