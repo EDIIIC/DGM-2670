ed1fc430200edd48d81b8df5bf824db16948cdf0
Ethan Cragun <craguned@gmail.com>
2018-11-23 02:25:09 -0700

    AutoDestroy and fps camera follow started

---
+++ Wild/Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+
+    public float lifetime;
+
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
