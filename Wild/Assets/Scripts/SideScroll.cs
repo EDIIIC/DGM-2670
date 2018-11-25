fe1af0940dbfc7cde40b1be385d0b7e6f8ce7dbd
Ethan Cragun <craguned@gmail.com>
2018-11-24 22:30:12 -0700

    Fixing issues

--- Wild/Assets/Scripts/SideScroll.cs
+++ Wild/Assets/Scripts/SideScroll.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GunMove : MonoBehaviour {
+public class SideScroll : MonoBehaviour {
 
 	public float Height;
 
