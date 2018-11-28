5ba182f711edb38e6ab29810b88361ad354739ad
Ethan Cragun <craguned@gmail.com>
2018-11-27 23:06:40 -0700

    Pause Menu
    and ignored a RunCountDown script that wouldn't stage. Lets see what that does

--- Re-2670/Assets/Scripts/ColorMatch/PurchasableObject.cs
+++ Re-2670/Assets/Scripts/ColorMatch/PurchasableObject.cs
@@ -4,16 +4,27 @@
 public class PurchasableObject : ScriptableObject
 {
     public Object Item;
+    public int UsageCount = 3;
     public int Value;
+    public bool Upgrade;
+    public PurchasableObject UpgradeFrom;
+    
     public bool Perpetual;
     public bool Instantiatable;
 
     public void CreateItem()
     {
-        if (Instantiatable && Item is GameObject)
+        for (int i = 0; i < UsageCount; i++)
         {
-            Instantiate(Item);
+            if (Instantiatable && Item is GameObject)
+            {
+                Instantiate(Item);
+            }
         }
     }
-    
+
+    public void Used()
+    {
+        UsageCount--;
+    }
 }
