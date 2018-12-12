41c69176d18adb76ac0e8eb4741afe6ee53656c5
Ethan Cragun <craguned@gmail.com>
2018-12-11 23:00:06 -0700

    AGHHHH
    Looking at the store and transferring to mac

--- Leap/Assets/Scripts/LevelScripts/PlatDestroy.cs
+++ Leap/Assets/Scripts/LevelScripts/PlatDestroy.cs
@@ -6,10 +6,10 @@ public class PlatDestroy : MonoBehaviour {
     
     private void OnCollisionEnter(Collision col)
     {
-       // if (col.gameObject.name == "platform(Clone)")
-       // {
+        if (col.gameObject.name == "platform(Clone)")
+        {
             ScoreBoard.scoreValue += 1;
             Destroy(col.gameObject);
-       // }
+        }
     }
 }
